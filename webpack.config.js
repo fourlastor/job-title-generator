19fdd1773ebe9f5ebe51a8660e251ec6f9202c30
Daniele Conti <dnlcnt@gmail.com>
2016-07-02 18:30:41 +0100

    build the whole thing with gulp

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, "dist"),
-        publicPath: "dist/",
         filename: "[name].js",
         chunkFilename: "[chunkhash].js"
     },
